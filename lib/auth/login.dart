41a0dfa1ca777d701f8d0375ece59e7e7e92cb7c
Oviquitor <victorgabrielcorreasilva0@gmail.com>
2022-12-21 22:00:17 -0400

    logo, e scroll

--- lib/auth/login.dart
+++ lib/auth/login.dart
@@ -73,133 +73,145 @@ class _LoginScreenState extends State<LoginScreen> {
         children: [
           Expanded(
             child: Container(
-              color: Colors.green,
+              //color: Colors.green,
+              //width: 300,
+              //height: 300,
+              //formas de definir o tamanho e ficar reponsivo
+              //padding: const EdgeInsets.all(100),
+              width: MediaQuery.of(context).size.width * 0.7,
+              height: MediaQuery.of(context).size.height * 0.3,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage("assets/images/OutSet2.png"),
+                  //fit: BoxFit.cover, --- ajusta a imagem nas condiçoes proposta
+                ),
+              ),
             ),
           ),
           Expanded(
-            child: Container(
-              padding: const EdgeInsets.symmetric(
-                horizontal: 32,
-                vertical: 20,
-              ),
-              decoration: const BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.vertical(
-                  top: Radius.circular(45),
+            child: SingleChildScrollView(
+              child: Container(
+                padding: const EdgeInsets.symmetric(
+                  horizontal: 32,
+                  vertical: 30,
                 ),
-              ),
-
-              //Texto Custom
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.stretch,
-                children: [
-                  //Email
-                  CustomText(
-                    icon: Icons.email,
-                    label: 'Email',
-                    controller: _emailController,
+                decoration: const BoxDecoration(
+                  color: Colors.white,
+                  borderRadius: BorderRadius.vertical(
+                    top: Radius.circular(45),
                   ),
+                ),
 
-                  //Senha
-                  CustomText(
-                    icon: Icons.lock,
-                    label: 'Senha',
-                    isSecret: true,
-                    controller: _passwordController,
-                  ),
+                //Texto Custom
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    //Email
+                    CustomText(
+                      icon: Icons.email,
+                      label: 'Email',
+                      controller: _emailController,
+                    ),
 
-                  //Botao Entrar
-                  SizedBox(
-                    height: 50,
-                    child: ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: Colors.black,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(18),
+                    //Senha
+                    CustomText(
+                      icon: Icons.lock,
+                      label: 'Senha',
+                      isSecret: true,
+                      controller: _passwordController,
+                    ),
+
+                    //Botao Entrar
+                    SizedBox(
+                      height: 50,
+                      child: ElevatedButton(
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Colors.black,
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(18),
+                          ),
+                        ),
+                        onPressed: () async {
+                          User? user = await loginUsingEmailPassword(
+                              email: _emailController!.text,
+                              password: _passwordController!.text,
+                              context: context);
+                          print(user);
+                          if (user != null) {
+                            Navigator.of(context).pushReplacement(
+                              MaterialPageRoute(builder: (context) => Home()),
+                            );
+                          }
+                        },
+                        child: const Text(
+                          'Entrar',
+                          style: TextStyle(
+                            fontSize: 18,
+                          ),
                         ),
                       ),
-                      onPressed: () async {
-                        User? user = await loginUsingEmailPassword(
-                            email: _emailController!.text,
-                            password: _passwordController!.text,
-                            context: context);
-                        print(user);
-                        if (user != null) {
-                          Navigator.of(context).pushReplacement(
-                            MaterialPageRoute(
-                              builder: (context) => Home()
+                    ),
+
+                    //Esqueceu a Senha
+                    Align(
+                      alignment: Alignment.centerRight,
+                      child: TextButton(
+                        onPressed: () {},
+                        child: const Text(
+                          'Esqueceu a senha?',
+                          style: TextStyle(
+                            color: Colors.red,
+                          ),
+                        ),
+                      ),
+                    ),
+
+                    //Divisor
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 10),
+                      child: Row(
+                        children: [
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey.withAlpha(90),
+                              thickness: 2,
                             ),
-                          );
-                        }
-                      },
-                      child: const Text(
-                        'Entrar',
-                        style: TextStyle(
-                          fontSize: 18,
-                        ),
+                          ),
+                          const Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 15),
+                            child: Text('Ou'),
+                          ),
+                          Expanded(
+                            child: Divider(
+                              color: Colors.grey.withAlpha(90),
+                              thickness: 2,
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                  ),
 
-                  //Esqueceu a Senha
-                  Align(
-                    alignment: Alignment.centerRight,
-                    child: TextButton(
-                      onPressed: () {},
-                      child: const Text(
-                        'Esqueceu a senha?',
-                        style: TextStyle(
-                          color: Colors.red,
-                        ),
-                      ),
-                    ),
-                  ),
-
-                  //Divisor
-                  Padding(
-                    padding: const EdgeInsets.only(bottom: 10),
-                    child: Row(
-                      children: [
-                        Expanded(
-                          child: Divider(
-                            color: Colors.grey.withAlpha(90),
-                            thickness: 2,
+                    //BotaoNovo
+                    SizedBox(
+                      height: 50,
+                      child: OutlinedButton(
+                        style: OutlinedButton.styleFrom(
+                          side: const BorderSide(
+                            width: 2,
+                            color: Colors.black,
                           ),
                         ),
-                        const Padding(
-                          padding: EdgeInsets.symmetric(horizontal: 15),
-                          child: Text('Ou'),
-                        ),
-                        Expanded(
-                          child: Divider(
-                            color: Colors.grey.withAlpha(90),
-                            thickness: 2,
+                        onPressed: () {},
+                        child: const Text(
+                          'Criar conta',
+                          style: TextStyle(
+                            color: Colors.black,
                           ),
                         ),
-                      ],
-                    ),
-                  ),
-
-                  //BotaoNovo
-                  SizedBox(
-                    height: 50,
-                    child: OutlinedButton(
-                      style: OutlinedButton.styleFrom(
-                        side: const BorderSide(
-                          width: 2,
-                          color: Colors.black,
-                        ),
-                      ),
-                      onPressed: () {},
-                      child: const Text(
-                        'Criar conta',
-                        style: TextStyle(
-                          color: Colors.black,
-                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
           ),
