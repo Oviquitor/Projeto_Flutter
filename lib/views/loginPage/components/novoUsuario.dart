76627eb9d516d8993e9e47a56e082ca9a4044b57
Oviquitor <victorgabrielcorreasilva0@gmail.com>
2023-02-07 19:49:05 -0400

    novo usuario

--- lib/views/loginPage/components/novoUsuario.dart
+++ lib/views/loginPage/components/novoUsuario.dart
@@ -98,7 +98,7 @@ class _NovoUsuarioState extends State<NovoUsuario> {
                               ),
                             );
                           } else {
-                            //cadastrar();
+                            cadastrar();
                           }
                         },
                         child: Text(
